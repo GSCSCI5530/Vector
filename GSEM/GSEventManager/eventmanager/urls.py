d7fc86e98e1abca5adf77e79b91096e316af7b0a
Josh <joshuakylelambert@gmail.com>
2018-10-12 20:32:15 -0400

    Changed the HML for the site

---
+++ GSEM/GSEventManager/eventmanager/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.event_list, name='event_list'),
+]
+
--- GSEM/GSEventManager/eventmanager/views.py
+++ GSEM/GSEventManager/eventmanager/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def event_list(request):
+    return render(request, 'eventmanager/event_list.html', {})
