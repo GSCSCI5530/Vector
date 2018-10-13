8439633b01645d5dfaee76c4e39c8a08b5fe6129
Josh <joshuakylelambert@gmail.com>
2018-10-12 23:00:15 -0400

    views and templates starting to work

--- GSEM/GSEventManager/eventmanager/views.py
+++ GSEM/GSEventManager/eventmanager/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Event
 
 # Create your views here.
 
 
 def event_list(request):
-    return render(request, 'eventmanager/event_list.html', {})
+    events = Event.objects.order_by('published_date')
+    return render(request, 'eventmanager/event_list.html', {'events': events})
