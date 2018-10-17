2930a21eef3e53ca02259aa75c975ce83b8b370d
Josh <joshuakylelambert@gmail.com>
2018-10-17 18:37:26 -0400

    create and edit forms from the page now

---
+++ GSEM/GSEventManager/eventmanager/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Event
+
+
+class EventForm(forms.ModelForm):
+
+    class Meta:
+        model = Event
+        fields = ('event_name', 'text',)
+
--- GSEM/GSEventManager/eventmanager/urls.py
+++ GSEM/GSEventManager/eventmanager/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('eventmanager/<int:pk>/', views.event_detail, name='event_detail'),
+    path('eventmanager/new', views.event_new, name='event_new'),
+    path('eventmanager/<int:pk>/edit', views.event_edit, name='event_edit'),
 ]
 
