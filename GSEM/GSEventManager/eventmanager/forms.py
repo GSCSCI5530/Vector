19df7dc4ba94737b853cd994b62061db762e348f
Josh <joshuakylelambert@gmail.com>
2018-10-30 17:48:34 -0400

    signup , login, logout

--- GSEM/GSEventManager/eventmanager/forms.py
+++ GSEM/GSEventManager/eventmanager/forms.py
@@ -6,5 +6,5 @@ class EventForm(forms.ModelForm):
 
     class Meta:
         model = Event
-        fields = ('event_name', 'text',)
+        fields = ('event_name', 'text', 'event_date', 'event_place')
 
