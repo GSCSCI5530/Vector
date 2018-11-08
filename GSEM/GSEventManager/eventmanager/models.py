532173a0ea72d5274c9b162ba0217735bad10aff
Josh <joshuakylelambert@gmail.com>
2018-11-07 21:12:48 -0500

    Edit and RSVP buttons only show up when logged in. RSVP button is replaced by "You are attending this event"  if the user has already RSVP'd

--- GSEM/GSEventManager/eventmanager/models.py
+++ GSEM/GSEventManager/eventmanager/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
@@ -15,8 +16,7 @@ class Event(models.Model):
         blank=True, null=True)
     event_date = models.DateTimeField(default=timezone.now)
     event_place = models.CharField(max_length=200)
-	
-	
+
     def post(self):
         self.published_date = timezone.now()
         self.save()
@@ -24,19 +24,21 @@ class Event(models.Model):
     def __str__(self):
         return self.event_name
 
+
 class Comment(models.Model):
-	event = models.ForeignKey('eventmanager.Event', on_delete=models.CASCADE, related_name='comments')
-	user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-	content = models.TextField(max_length=160)
-	timestamp = models.DateTimeField(auto_now_add=True)
-	approved = models.BooleanField(default=False)
-	
-	def approve(self):
-		self.approved = True
-		self.save()
-		
-	def __str__(self):
-		return self.content
+    event = models.ForeignKey('eventmanager.Event', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    content = models.TextField(max_length=160)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
+
+    def __str__(self):
+        return self.content
+
 
 class Attendee(models.Model):
     # event = models.ForeignKey('Event', on_delete=models.CASCADE)
@@ -47,4 +49,3 @@ class Attendee(models.Model):
 
     def attend(self):
         self.save()
-
